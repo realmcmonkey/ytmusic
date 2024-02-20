d49661237547987587d87374ef589607eaa42264
NovusTheory <3434404+NovusTheory@users.noreply.github.com>
2024-02-19 22:52:58 -0600

    🩹 Remove indented from enhanced media session setting

--- src/renderer/windows/settings/Settings.vue
+++ src/renderer/windows/settings/Settings.vue
@@ -433,7 +433,6 @@ window.ytmd.handleUpdateDownloaded(() => {
           <YTMDSetting
             v-model="enhancedMediaServiceEnabled"
             type="checkbox"
-            indented
             name="Enable enhanced media service"
             description="An enhanced media service that can provide better media button controls"
             @change="settingChangedRequiresRestart"
